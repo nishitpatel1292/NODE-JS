c0f23eeb35c8cc49f3167c4526e549b4f43689a2
nishitpatel1292 <73834549+nishitpatel1292@users.noreply.github.com>
2023-09-16 23:34:15 +0530

    fsModule added

--- fsModule.js
+++ fsModule.js
@@ -1,9 +1,37 @@
-const { readFileSync, writeFileSync } = require('fs')
+// const { readFileSync, writeFileSync } = require('fs')
 
-const first = readFileSync('./content/first.txt', 'utf-8')
-const second = readFileSync('./content/second.txt', 'utf-8')
+// const first = readFileSync('./content/first.txt', 'utf-8')
+// const second = readFileSync('./content/second.txt', 'utf-8')
 
-//this will add new file with specified name if it does not exist.
-//if file exists with the same name then content will be replaced
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`)
+// //this will add new file with specified name if it does not exist.
+// //if file exists with the same name then content will be replaced
+// writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`)
 
+const { readFile, writeFile } = require('fs')
+
+readFile('./content/first.txt', 'utf-8',
+    (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const first = result;
+        readFile('./content/second.txt', 'utf-8',
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                const second = result
+                writeFile('./content/result_async.txt', `Here is the aysnc result: ${first}, ${second}`,
+                    (err, result) => {
+                        if (err) {
+                            console.log(err)
+                            return;
+                        }
+                        console.log(result)
+                    })
+            })
+
+
+    })
